e0f19365d3ef666dc443902545eb40f37d7c6df9
yasuo <yasuo@nttr.co.jp>
2012-09-30 23:21:30 +0900

    Initial Commit

---
+++ EkiBuru/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EkiBuru
+//
+//  Created by yusuke_yasuo on 2012/09/30.
+//  Copyright (c) 2012年 yusuke_yasuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EkiBuru/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EkiBuru
+//
+//  Created by yusuke_yasuo on 2012/09/30.
+//  Copyright (c) 2012年 yusuke_yasuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
